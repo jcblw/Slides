4dd86ccb2cd3265ba26323a48a35700471d4afa1
jacoblwe20 <jacoblowe2.0@gmail.com>
2012-05-07 20:00:03 -0600

    adding new content for plugged into jquery

--- html5slides-read-only/plugged-into-jquery/script.js
+++ html5slides-read-only/plugged-into-jquery/script.js
@@ -1,41 +1,97 @@
-  
-//Demo Two
-(function(){
-    var ele = document.querySelector('.demo_two');
-    
-    ele.addEventListener('click', function(){
-        ele.className = ele.className + ' clicked';
-    })
-}());
-  
-  //Demo Three
-  
-(function(){
-    //using html 5
-    var ele = document.querySelector('.demo_three');
-    
-    ele.addEventListener('click', function(){
-        this.style.background = 'rgba(0,0,0,.3)';  
-    });
-}());
-  
-  
-//Detection script
-
-var supportTransition;
-(function(){
-    supportTransition = function (){
-        //Test for each vendor and W3C properties 
-        if(	'WebkitTransition' in document.body.style ||
-            'MozTransition' in document.body.style ||
-            'OTransition' in document.body.style ||
-            'MsTransition' in document.body.style ||
-            'transition' in document.body.style){
-              return true;
+//Fullscreen
+// via http://johndyer.name/native-fullscreen-javascript-api-plus-jquery-plugin/
+(function() {
+    var
+        fullScreenApi = {
+            supportsFullScreen: false,
+            isFullScreen: function() { return false; },
+            requestFullScreen: function() {},
+            cancelFullScreen: function() {},
+            fullScreenEventName: '',
+            prefix: ''
+        },
+        browserPrefixes = 'webkit moz o ms khtml'.split(' ');
+ 
+    // check for native support
+    if (typeof document.cancelFullScreen !== 'undefined') {
+        fullScreenApi.supportsFullScreen = true;
+    } else {
+        // check for fullscreen support by vendor prefix
+        for (var i = 0, il = browserPrefixes.length; i < il; i++ ) {
+            fullScreenApi.prefix = browserPrefixes[i];
+ 
+            if (typeof document[fullScreenApi.prefix + 'CancelFullScreen' ] !== 'undefined' ) {
+                fullScreenApi.supportsFullScreen = true;
+ 
+                break;
             }
-        
-        return false;
-    }   
+        }
+    }
+ 
+    // update methods to do something useful
+    if (fullScreenApi.supportsFullScreen) {
+        fullScreenApi.fullScreenEventName = fullScreenApi.prefix + 'fullscreenchange';
+ 
+        fullScreenApi.isFullScreen = function() {
+            switch (this.prefix) {
+                case '':
+                    return document.fullScreen;
+                case 'webkit':
+                    return document.webkitIsFullScreen;
+                default:
+                    return document[this.prefix + 'FullScreen'];
+            }
+        };
+        fullScreenApi.requestFullScreen = function(el) {
+            return (this.prefix === '') ? el.requestFullScreen() : el[this.prefix + 'RequestFullScreen']();
+        };
+        fullScreenApi.cancelFullScreen = function(el) {
+            return (this.prefix === '') ? document.cancelFullScreen() : document[this.prefix + 'CancelFullScreen']();
+        };
+    }
+ 
+    // jQuery plugin
+    if (typeof jQuery !== 'undefined') {
+        jQuery.fn.requestFullScreen = function() {
+ 
+            return this.each(function() {
+                if (fullScreenApi.supportsFullScreen) {
+                    fullScreenApi.requestFullScreen(this);
+                }
+            });
+        };
+    }
+ 
+    // export api
+    window.fullScreenApi = fullScreenApi;
+})();
+
+
+(function(){
+	var btn = document.querySelector('.fullscreen');
+	if (fullScreenApi.supportsFullScreen) {
+		var enterFullscreen = function(){
+			// Go full screen
+			fullScreenApi.requestFullScreen(document.getElementsByTagName('body')[0]);
+			
+			//Add new handle to button
+			btn.removeEventListener('click', enterFullscreen, true);
+			btn.addEventListener('click', exitFullscreen, true);
+		},
+		exitFullscreen = function(){
+			fullScreenApi.cancelFullScreen(document.getElementsByTagName('body')[0]);
+			
+			//Add new handle to button
+			btn.removeEventListener('click', exitFullscreen, true);
+			btn.addEventListener('click', enterFullscreen, true);
+			
+		};
+		btn.addEventListener('click', enterFullscreen, true);
+	}else{
+		btn.innerHTML = 'Boo no fullscreen';
+		btn.classname = btn.classname + ' disabled';	
+	}
+	
 }());
 
 // Jquery Fallback
